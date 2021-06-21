2c1b8f87fc5ffb21caa5a2460637c957b0ff30f6
alex <alexmihalyk23@gmail.com>
2021-06-21 12:19:52 +0700

    update

--- main.py
+++ main.py
@@ -2,29 +2,49 @@ import pyvirtualcam
 import numpy as np
 import cv2
 face_cascade = cv2.CascadeClassifier("haar.xml")
+yROI = 80
+yMaxROI = 400
+xROI = 80
+xMaxROI = 560
+# imCrop = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+CenterFace = [(xROI + xMaxROI) // 2, (yROI + yMaxROI) // 2]
 with pyvirtualcam.Camera(width=640, height=480, fps=20) as cam:
     cap = cv2.VideoCapture(0)
     print(f'Using virtual camera: {cam.device}')
     # frame = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
     while True:
         ret, frame = cap.read()
-        #
+        ROI = frame.copy()
 
-        ret, frame = cap.read()
+        CenterROI = [(xROI + xMaxROI) // 2, (yROI + yMaxROI) // 2]
 
         faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=12)
-        print(faces)
+
         for x, y, w, h in faces:
-            # frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
             if x > 100 and y > 100:
-                frame = frame[y - 100:y + h + 100, x - 100:x + w + 100]
-            elif y+h+100 > 480 or x+w+100 >640:
-                frame = frame[y:y + h, x:x + w]
+                crop_img = frame[y - 100:y + h + 100, x - 100:x + w + 100]
             else:
-                frame = frame[y:y + h+100, x:x + w+100]
+                crop_img = frame[y:y + h + 100, x + 100:x + w + 100]
+
+            CenterFace = [(x + x + w) // 2, (y + y + h) // 2]
+            frame = cv2.circle(frame, CenterFace, 1, (244, 22, 244))
+        if not (CenterFace[0] in range(CenterROI[0] - 5, CenterROI[0] + 10) and CenterFace[1] in range(CenterROI[1] - 5,
+                                                                                                       CenterROI[
+                                                                                                        1] + 10)):
+            xROI = xROI + (CenterFace[0] - CenterROI[0])
+            yROI = yROI + (CenterFace[1] - CenterROI[1])
+            yMaxROI = yMaxROI + (CenterFace[1] - CenterROI[1])
+            xMaxROI = xMaxROI + (CenterFace[0] - CenterROI[0])
+
+        if xROI < 0 or yROI < 0:
+            ROI = frame[80:yMaxROI, 80:xMaxROI]
+        else:
+            ROI = frame[yROI:yMaxROI, xROI:xMaxROI]
 
         cv2.waitKey(0)
         # frame = cv2.resize(frame,(1280,720))
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.cvtColor(ROI, cv2.COLOR_BGR2RGB)
         cam.send(cv2.resize(frame,(640,480)))
         cam.sleep_until_next_frame()
