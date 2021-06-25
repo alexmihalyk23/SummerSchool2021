4f04bae1771ef1ae393aad6dc3968128639aaa94
alex <alexmihalyk23@gmail.com>
2021-06-25 22:20:58 +0700

    bug fix

--- facefilter.py
+++ facefilter.py
@@ -2,27 +2,12 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier("haar.xml")
-mask = cv2.imread("test.png", -1)
-# mask1 = mask.copy()
-# mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-# ret, mask = cv2.threshold(mask_gray, 25, 255, cv2.THRESH_BINARY)
-# mask = cv2.cvtColor(mask, cv2.COLOR_BGRA2BGR)
-def transparentOverlay(src, overlay , pos = (0,0)  , scale = 1):
-    overlay = cv2.resize(overlay , (0,0) ,fx = scale , fy = scale)
-    h , w , _ =  overlay.shape ## size of foreground image
-    rows , cols , _ = src.shape  ## size of background image
-    y , x = pos[0] , pos [1]
+
+# rows, cols, channels = mask_img.shape
 
 
-    for i in range(h):
-        for j in range(w):
-            if x + i > rows or y + j >=cols:
-                continue
-            alpha = float(overlay[i][j][3]/255) ##  read the alpha chanel
-            src[x+i][y+j] = alpha * overlay[i][j][:3] + (1-alpha) * src[x+i][y+j]
-    return src
 
-
+# Now black-out the area of logo in ROI
 
 while True:
 
@@ -30,18 +15,47 @@ while True:
 
     # CODE HERE
     faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=12)
+
     # в нашей переменной теперь хранятся хначения координат лица
+
+
+    # threshold это пороговое значение
+    # это значит что все пиксели, которые меньше порога становятся 0 а все что больше - 255
+    # для того чтобы это работало корректно, сначала нам нужно перевести изображение в оттенки серого
+    mask_img = cv2.imread("mask.png")
+    img2gray = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 20, 255, cv2.THRESH_BINARY)
     for (x, y, w, h) in faces:
-        if h > 0 and w > 0:
+        # print(rows,cols,w,h)
+        roi = frame[y+40:y+40+h, x:x+w]
+        # mask_img = cv2.resize(mask_img,(w,h))
+        # mask1 = mask.copy()
 
-            mask_symin = int(y+0.1 * h / 6)
-            mask_symax = int(y + 8* h / 6)
-            sh_mask = mask_symax - mask_symin
+        mask = cv2.resize(mask,(w,h))
+        mask_img = cv2.resize(mask_img,(w,h))
+        mask_inv = cv2.bitwise_not(mask)
+        # Now black-out the area of logo in ROI
+        img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+        # Take only region of logo from logo image.
+        img2_fg = cv2.bitwise_and(mask_img, mask_img, mask=mask)
+        # Put logo in ROI and modify the main image
+        dst = cv2.add(img1_bg, img2_fg)
 
-            face_glass_ori = frame[mask_symin:mask_symax, x:x + w]
 
-            mask = cv2.resize(mask, (w, sh_mask))
-            transparentOverlay(face_glass_ori, mask)
+        dst = cv2.resize(dst, (w,h))
+        frame[y+40:y+40+h, x:x+w] = dst
+    # for (x, y, w, h) in faces:
+    #     mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    #     ret, mask = cv2.threshold(mask_gray, 25, 255, cv2.THRESH_BINARY)
+    #     mask_inv = cv2.bitwise_not(mask)
+    #     ROI = frame[y:y+h,x:x+w]
+    #     img1_bg = cv2.bitwise_and(ROI, ROI, mask=mask_inv)
+    #     # Take only region of logo from logo image.
+    #     img2_fg = cv2.bitwise_and(frame, frame, mask=mask)
+    #     # Put logo in ROI and modify the main image
+    #     dst = cv2.add(img1_bg, img2_fg)
+    #     frame[y:y+h, x:x+h] = dst
+
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
     cv2.imshow("img", frame)
--- lesson_1.py
+++ lesson_1.py
@@ -129,3 +129,30 @@ while(1):
 cv2.destroyAllWindows()
 
 
+#############
+
+# img1 = cv2.imread('C:/Users/alexm/Pictures/english_1.png')
+# img2 = cv2.imread('test.png')
+# img2 = cv2.resize(img2, (160,120))
+# # I want to put logo on top-left corner, So I create a ROI
+# rows,cols,channels = img2.shape
+# roi = img1[0:rows, 0:cols]
+# # Now create a mask of logo and create its inverse mask also
+# img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+# # threshold это пороговое значение
+# # это значит что все пиксели, которые меньше порога становятся 0 а все что больше - 255
+# # для того чтобы это работало корректно, сначала нам нужно перевести изображение в оттенки серого
+# ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+# print(mask)
+# mask_inv = cv2.bitwise_not(mask)
+# print(mask_inv)
+# # Now black-out the area of logo in ROI
+# img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+# # Take only region of logo from logo image.
+# img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+# # Put logo in ROI and modify the main image
+# dst = cv2.add(img1_bg,img2_fg)
+# img1[0:rows, 0:cols ] = dst
+# cv2.imshow('res',img1)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
